8a1d69fe06ec333a8ff2973525b8548dcb35aee0
yejin <whdpwls10@gmail.com>
2016-07-13 14:58:44 +0900

    templates to display subs from database.

--- first/views.py
+++ first/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Sub
+
 
 def sub_list(request):
-	return render(request, 'first/sub_list.html', {})
+	subs = Sub.objects.order_by('name')
+	return render(request, 'first/sub_list.html', {'subs':subs})
 
 # Create your views here.
